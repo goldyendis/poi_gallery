fe36c817753415399a80d9f5d38134a780dd7ded
goldyendis <goldyendis@gmail.com>
2023-10-26 01:06:32 +0200

    20231026

--- backend/poiAPI/serializers.py
+++ backend/poiAPI/serializers.py
@@ -27,9 +27,10 @@ class POICategorySerializer(serializers.Serializer):
 class POIListItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = POI
-        fields = ["objectid", "surveydate","poicat","poitype","notes","poiname","poi_id","img_flag","existing","shape","thumbnail"]
+        fields = ["objectid", "surveydate","poicat","poitype","notes","poiname","poi_id","img_flag","existing","shape","thumbnail","images_list"]
     
     thumbnail = serializers.SerializerMethodField()
+    images_list = serializers.SerializerMethodField()
     
     def get_thumbnail(self, obj:POI):
         '''Always get back the newest image as a thumbnail image'''
@@ -40,3 +41,13 @@ class POIListItemSerializer(serializers.ModelSerializer):
             image_names = os.listdir(folder_path)
             newest_image_full_path = os.path.join("https://turistaterkepek.hu/poiimages/",obj.poi_id,image_names[0])
         return newest_image_full_path
+    
+    def get_images_list(self, obj:POI):
+        image_folder = r'\\192.168.121.2\images'
+        folder_path = os.path.join(image_folder,obj.poi_id)
+        full_image_names = []
+        if os.path.exists(folder_path) and os.path.isdir(folder_path):
+            image_names = []
+            image_names = os.listdir(folder_path)
+            full_image_names = [os.path.join("https://turistaterkepek.hu/poiimages/",obj.poi_id,image_name) for image_name in image_names]
+        return full_image_names
